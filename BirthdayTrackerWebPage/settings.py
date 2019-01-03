6ca06b584d59e5d6f1072e6ca3d0da3490fe5e8f
Zahra Naghibi Nasab <naghibinasab@mail.um.ac.ir>
2019-01-03 14:16:26 +0330

    models.py

--- BirthdayTrackerWebPage/settings.py
+++ BirthdayTrackerWebPage/settings.py
@@ -73,17 +73,17 @@ WSGI_APPLICATION = 'BirthdayTrackerWebPage.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'birthdaytracker',
-#         'USER': 'root',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306'
 #
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '‌hospitalman',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+
+    }
+}
 
 
 # Password validation
--- BirthdayTrackersite/models.py
+++ BirthdayTrackersite/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Events(models.Model):
+    day = models.IntegerField()
+    month =models.IntegerField()
+    year = models.IntegerField()
+    event = models.TextField(max_length=200)
+
+class Birth(models.Model):
+    day = models.IntegerField()
+    month =models.IntegerField()
+    year = models.IntegerField()
+    event = models.TextField(max_length=200)
+
+class Death(models.Model):
+    day = models.IntegerField()
+    month =models.IntegerField()
+    year = models.IntegerField()
+    event = models.TextField(max_length=200)
